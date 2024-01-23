65fbd1300b502ee3eed06182bd17dd39daae29d5
raju-islam <rajuslam39@gmail.com>
2024-01-23 22:17:52 +0600

    home page working

---
+++ lib/utills/round_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+class RoundButton extends StatelessWidget {
+   RoundButton({super.key, required this.name,  this.color=Colors.indigo,  this.hieght=60,  this.width=200, required this.onpress});
+  final String name;
+  final Color color;
+  final double hieght;
+  final double width;
+  final VoidCallback onpress;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return     InkWell(onTap: onpress,
+      child: Container(
+        width: width,
+        height: hieght,
+        decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(15)
+        ),child: Center(
+        child: Text("Get Started",style: TextStyle(color: Colors.white,fontSize: 30),),
+      ),
+      ),);
+  }
+}
--- lib/view/intro_page.dart
+++ lib/view/intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medical_app_ui/utills/round_button.dart';
 import 'package:medical_app_ui/view/home_page.dart';
 class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
@@ -33,19 +34,9 @@ class _IntroPageState extends State<IntroPage> {
               child: Text("Find your doctor and make an appointment with one tap",style: TextStyle(fontSize: 25),),
             ),
             Spacer(),
-            InkWell(onTap: (){
+            RoundButton(name: "Get Started", onpress: (){
               Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
-            },
-            child: Container(
-              width: width*0.9,
-              height: hieght*0.07,
-              decoration: BoxDecoration(
-                color: Colors.indigo,
-                borderRadius: BorderRadius.circular(15)
-              ),child: Center(
-              child: Text("Get Started",style: TextStyle(color: Colors.white,fontSize: 30),),
-            ),
-            ),),
+            },hieght: hieght*0.08,width: width*0.7,),
             Spacer(),
           ],
         ),
